5d0dfd1ed622a1fb545f1fa4a35691301e86d91c
ndstrzz <andynilessim05@gmail.com>
2025-10-22 15:58:55 +0800

    physical 1.8

--- app/src/routes/create/CreateArtwork.tsx
+++ app/src/routes/create/CreateArtwork.tsx
@@ -460,8 +460,8 @@ if (step === 0) {
           <img
             src="/images/digital-icon.svg"
             alt=""
-            width={40}
-            height={40}
+            width={80}
+            height={80}
             className="h-10 w-10 mb-3"
             loading="eager"
           />
